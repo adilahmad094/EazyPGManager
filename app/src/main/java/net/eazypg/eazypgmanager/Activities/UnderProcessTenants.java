d6949fd5664e14d476b87e888ed788c0dc739d0e
Vardaan <vardaan13121997@gmail.com>
2018-12-05 18:22:34 +0530

    Underprocess backButton

--- app/src/main/java/net/eazypg/eazypgmanager/Activities/UnderProcessTenants.java
+++ app/src/main/java/net/eazypg/eazypgmanager/Activities/UnderProcessTenants.java
@@ -1,6 +1,7 @@
 package net.eazypg.eazypgmanager.Activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,9 +10,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.dynamite.DynamiteModule;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -45,8 +49,14 @@ public class UnderProcessTenants extends AppCompatActivity {
     RecyclerView underprocessRecyclerView;
 
     Context context;
-
+    ImageView backButton;
     UnderprocessDetailList underprocessDetailList;
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(UnderProcessTenants.this, TenantActivity.class));
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +73,18 @@ public class UnderProcessTenants extends AppCompatActivity {
         listView.setEmptyView(emptyList);
 */
         tenantDetailsList = new ArrayList<>();
+
+        backButton = findViewById(R.id.imageView3);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UnderProcessTenants.this, TenantActivity.class));
+                finish();
+            }
+        });
+
+
+
 
         underprocessRecyclerView = findViewById(R.id.UnderProcessTenantRecyclerView);
         context = UnderProcessTenants.this;
